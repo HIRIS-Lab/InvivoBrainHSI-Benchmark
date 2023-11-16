5a608ebd096cf936710a38bd788840c1afbe651d
Raquel Leon <106959110+slmartin1@users.noreply.github.com>
2023-11-16 12:52:10 +0000

    Add files via upload

---
+++ main.m
@@ -0,0 +1,67 @@
+% MAIN Entry point for executing an example of the spectralSpatialMV function.
+%
+%   This script demonstrates the usage of the spectralSpatialMV function.
+%   This function takes a hyperspectral data cube (hsCube) and a model for
+%   classification (model), and performs spectral-spatial framework that
+%   include classification (classification), PCA reduction (pca_r),
+%   K-nearest neighbors (knn) filtering, HKM segmentation (segmentation),
+%   majority voting value (mv), and density maps (tmd).
+%
+%   Authors:  Raquel Leon, Himar Fabelo, Samuel Ortega
+%
+%   Email address:
+%   slmartin@iuma.ulpgc.es, hfabelo@iuma.ulpgc.es, sortega@iuma.ulpgc.es
+
+%   See also: spectralSpatialMV
+
+%% Example using a pre-trained model 
+
+% Clear workspace and command window
+clear all;
+clc;
+addpath('utils/');
+
+%Load the HS Cube
+load("data\Op42C2.mat", "hsCube");
+
+%Load the model 
+%First, include the model in the folder. Change the variable name
+%if is necessary
+
+load("models\model_RF.mat", "model");
+
+%Compute the spatial-spectral framework
+[classification, pca_r, knn, clusterMap, mv, tmdMap ] = spectralSpatialMV(model, hsCube);
+
+%Plot results
+plotExample(classification, pca_r, knn, clusterMap, mv, tmdMap);
+
+rmpath('utils/');
+%% Example using predicted labels and probabilities
+
+% Clear workspace and command window
+clear all;
+clc;
+
+addpath('utils/');
+%Load the HS Cube
+load("data\Op42C2.mat", "hsCube");
+
+%Load the predicted labels and probabilities
+%First, include the probabilities in the folder. Change the variable name
+%if is necessary
+
+load("models\svmProb.mat", "model");
+%Employ the following struct:
+%     model.probAllImage: probabilities
+%     model.classMap: predicted labels 
+%     model.listOfClass: probabilities order
+
+%Compute the spatial-spectral framework
+[classification, pca_r, knn, clusterMap, mv, tmdMap ] = spectralSpatialMV(model, hsCube);
+
+%Plot results
+plotExample(classification, pca_r, knn, clusterMap, mv, tmdMap);
+
+rmpath('utils/');
+% End of main script
--- spectralSpatialMV.m
+++ spectralSpatialMV.m
@@ -1,63 +1,160 @@
-% Main
+function [classification, pca_r, knn, clusterMap, mv, tmdMap ] = spectralSpatialMV(model, hsCube)
+% SPECTRALSPATIALMV Performs spectral-spatial classification.
+%
+%   [classification, pca_r, knn, segmentation, mv, tmd] =
+%   spectralSpatialMV(model, hsCube)
+%
+%   This function takes a hyperspectral data cube (hsCube) and a model for
+%   classification (model), and performs spectral-spatial framework that
+%   include classification (classification), PCA reduction (pca_r),
+%   K-nearest neighbors (knn) filtering, HKM segmentation (segmentation),
+%   majority voting value (mv), and density maps (tmd).
+%
+%   INPUTS:
+%   - model: (obj or struct) 'TreeBagger', 'ClassificationKNN',
+%   'ClassificationECOC', 'SeriesNetwork' classification model (e.g., SVM,
+%   Random Forest) or Struct with 4-class probability and predicted label for hyperspectral data.    
+%       - probAllImage: 4-class probability
+%       - classMap: predicted labels
+%       - listOfClass: probability order
+%   - hsCube: (double) 3D hyperspectral cube to be analyzed.
+%
+%   OUTPUTS:
+%   - classification: (struct) results of the classification usign the model.
+%       - classMap: predicted labels
+%       - probAllImage:  4-class probability
+%       - colorMap: 4-class RGB color map
+%   - pca_r: (struct) Principal Component Analysis reduction results.
+%       - pcaCube: Principal component scores
+%       - pcaComp1: Principal Component
+%   - knn: (struct) K-nearest neighbors filtering results.
+%       - mapProb: 4-class probability
+%       - classMap: predicted labels
+%       - colorMap: 4-class RGB color map
+%   - clusterMap: Image segmentation results.
+%   - mv: (struct) majority voting results.
+%       - mapProb: 4-class RGB probability
+%       - classMap: predicted labels
+%       - colorMap: 4-class RGB color map
+%   - tmdMap: density maps results.
+%
+%   See also: pca, predict, classify
+%
+%   Authors:  Raquel Leon, Himar Fabelo, Samuel Ortega
+%
+%   Email address:
+%   slmartin@iuma.ulpgc.es, hfabelo@iuma.ulpgc.es, sortega@iuma.ulpgc.es
 
-function [pca, knn, segmentation, mv, tmd] = spectralSpatialMV(model, hsCube)
+
 rng('default') % For reproducibility
+addpath(genpath("utils\"));
 
-%Transform the HSCube to 2D
-hcSize = size(hsCube);
-vectorizedCube = reshape(hsCube, [hcSize(1)*hcSize(2) hcSize(3)]);
+% Check toolbox dependency
+if sum("Statistics and Machine Learning Toolbox" == matlab.addons.installedAddons().Name)
+    disp('All Toolbox required installed');
+else
+    error ('Statistics and Machine Learning Toolbox Missing, install it before continuing.');
+end
 
+
+% Check the dimension of hsCube
+if ndims(hsCube) == 3
+    % Reshape the 3D matrix to 2D
+    hcSize = size(hsCube);
+    vectorizedCube = reshape(hsCube, [hcSize(1)*hcSize(2) hcSize(3)]);
+else
+    error('Input hsCube variable must be a 3D matrix');
+end
+
+
+%% Probabilities generation
+disp('***********Computing Classification...***********');
+
+if isstruct(model) %Check if model only contains the probabilities
+    listOfClass = model.listOfClass;
+    classification.probAllImage = model.probAllImage;
+    classification.classMap = model.classMap;
+
+elseif isobject(model) %Check if model is a pre-trained model
+    algorithmName = class(model);
+    % Obtain the probability scores  amd predicted label for each class in each pixel.
+    switch algorithmName
+        case {'TreeBagger', 'ClassificationKNN', 'ClassificationECOC'}
+            [classification.classMap, classification.probAllImage] = predict(model, vectorizedCube);
+
+            if isnumeric(model.ClassNames)
+                listOfClass = model.ClassNames;
+            else
+                listOfClass = str2double(model.ClassNames); %For TreeBagger Models
+            end
+
+            if ~isnumeric(classification.classMap) %For TreeBagger Models
+                classification.classMap = str2double(classification.classMap);
+            end
+
+        case 'SeriesNetwork'
+            [classification.classMap, classification.probAllImage] = classify(model, vectorizedCube);
+            classification.classMap = single(classification.classMap);
+
+            listOfClass = [1,2,3,4];
+
+        otherwise
+            error(['Unexpected model type. Model must be TreeBagger, ClassificationKNN,' ...
+                ' ClassificationECOC or SeriesNetwork.'])
+    end
+end
+
+% Reshape the 3D matrix to 2D
+classification.probAllImage = reshape(classification.probAllImage, [hcSize(1), hcSize(2),4]);
+classification.classMap = reshape(classification.classMap, [hcSize(1), hcSize(2),1]);
+
+clear model algorithmName
+%% PCA
 % PCA Generation
 disp('***********Computing PCA...***********');
-[pca.coeff, pca.pcavector, pca.latent, pca.tsquared] = pca(vectorizedCube);
-pca.pcaMap = reshape(pca.pcavector, [hcSize(1), hcSize(2), hcSize(3)]);
-pca.pca1 = pca.pcaMap(:,:,1);
 
-% Load supervised model and probabilities
-probMapR = model.ProbAllImage;
+warning('off', 'stats:pca:ColRankDefX')
+[~, pcavector, ~] = pca(vectorizedCube); %CHECK OUTPUT
+pca_r.pcaCube = reshape(pcavector, [hcSize(1), hcSize(2), hcSize(3)]);
+%Get the first component
+pca_r.pcaComp1 = pca_r.pcaCube(:,:,1);
 
+clear pcavector
 %% KNN
 % KNN Filtering Algorithm Parameters:
 disp('***********Computing KNN...***********');
 lambdaValue = 1;
 kValue = 40;
 
-Knn.mapProb = knnFilter_window(probMapR, pca.pca1, lambdaValue, kValue);
+knn.mapProb = knnFilter_window(classification.probAllImage, pca_r.pcaComp1, lambdaValue, kValue);
 
-listOfClass = model.Label;
-knn.map = zeros(hcSize(1), hcSize(2));
+%Generate a 4 class map
+knn.classMap = zeros(hcSize(1), hcSize(2));
+[~, currentClassIndex] = max(knn.mapProb, [], 3);
+knn.classMap = listOfClass(currentClassIndex);
 
-for z = 1 : hcSize(1)
-    for j = 1 : hcSize(2)
-        [~, currentClassIndex] = max( knn.mapProb(z, j, :) );
-        if isnumeric( listOfClass(currentClassIndex) )
-            knn.map(z,j) = listOfClass(currentClassIndex);
-        else
-            knn.map(z,j) = str2num( cell2mat( listOfClass(currentClassIndex) ) );
-        end
-    end
-end
-
-%% HKM
+clear lambdaValue kValue currentClassIndex
+%% HKM ----- EVALUAR
 % Clustering Algorithm Parameters:
 disp('***********Computing HKM...***********');
-clusteringAlgoritmName = 'hkm';
 numberOfClusters = 24;
 
-segmentation.map = hypercubeClustering(hsCube, clusteringAlgoritmName,...
-    numberOfClusters);
+[clusterMap] = hierclust2nmfMulti(hsCube,numberOfClusters);
 
-
+clear clusteringAlgoritmName numberOfClusters
 %% MV
 disp('***********Computing Majority Voting...***********');
-[mv.map, mv.mapProb] = majorityVoting(knn.map, segmentation.map);
+[mv.classMap, mv.mapProb] = majorityVoting(knn.classMap, clusterMap);
 
 %% TMD
 disp('***********Computing TMD...***********');
-[tmd.map,tmd.omdMap] = generateDensityMaps(mvMap, mvMapProb);
+[tmdMap] = generateColorMap(mv, 'density');
 
 %% Generate color maps
+disp('***********Generating color maps...***********');
+knn.colorMap = generateColorMap(knn.classMap, 'fourClass');
+mv.colorMap = generateColorMap(mv.classMap, 'fourClass' );
+classification.colorMap = generateColorMap(classification.classMap, 'fourClass');
 
-knn.ColorMap = generateColorMap(knn.map );
-mv.ColorMap = generateColorMap( mv.map );
+rmpath(genpath("utils\"));
 end
